28c89d411a2aaaa8215071890ff7f24377567cb6
Toktam <ttmohebbi@gmail.com>
2022-04-20 15:51:36 -0400

    create my-counter component.ts

---
+++ src/app/my-counter/my-counter.component.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Component({
+  selector: 'app-my-counter',
+  templateUrl: './my-counter.component.html',
+})
+export class MyCounterComponent {
+  count$: Observable<number>;
+
+  constructor() {
+    // TODO: Connect 'this.count$' stream to the current store 'count' state
+  }
+
+  increment() {
+    // TODO: Dispatch an increment action
+  }
+
+  decrement() {
+    // TODO: Dispatch a decrement action
+  }
+
+  reset() {
+    // TODO: Dispatch a reset action
+  }
+}
