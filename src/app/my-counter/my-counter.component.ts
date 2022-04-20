241e919d0460e606ac26ffe305b64a55c17e0eaf
Toktam <ttmohebbi@gmail.com>
2022-04-20 16:02:22 -0400

    inject the store into MyCounterComponent and connect the count$ stream to the store's count state

--- src/app/my-counter/my-counter.component.ts
+++ src/app/my-counter/my-counter.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { Observable } from 'rxjs';
+import { Store } from '@ngrx/store';
+import { increment, decrement, reset } from '../counter.actions';
 
 @Component({
   selector: 'app-my-counter',
@@ -8,7 +10,7 @@ import { Observable } from 'rxjs';
 export class MyCounterComponent {
   count$: Observable<number>;
 
-  constructor() {
+  constructor(private store: Store<{ count: number }>) {
     // TODO: Connect 'this.count$' stream to the current store 'count' state
   }
 
