a5cd7d4b6904ab628da1833ba734090fd977bd25
Toktam <ttmohebbi@gmail.com>
2022-04-20 16:14:48 -0400

    implement increment, decrement and reset methods by dispatching actions to the store

--- src/app/my-counter/my-counter.component.ts
+++ src/app/my-counter/my-counter.component.ts
@@ -11,19 +11,18 @@ export class MyCounterComponent {
   count$: Observable<number>;
 
   constructor(private store: Store<{ count: number }>) {
-    // TODO: Connect 'this.count$' stream to the current store 'count' state
     this.count$ = store.select('count');
   }
 
   increment() {
-    // TODO: Dispatch an increment action
+    this.store.dispatch(increment());
   }
 
   decrement() {
-    // TODO: Dispatch a decrement action
+    this.store.dispatch(decrement());
   }
 
   reset() {
-    // TODO: Dispatch a reset action
+    this.store.dispatch(reset());
   }
 }
