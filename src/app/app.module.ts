d5e42aa4d444c44045f0911cb477342119ed8fc4
Toktam <ttmohebbi@gmail.com>
2022-04-20 15:56:50 -0400

    add MyCounterComponent to the app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,9 +6,10 @@ import { AppComponent } from './app.component';
 
 import { StoreModule } from '@ngrx/store';
 import { counterReducer } from './counter.reducer';
+import { MyCounterComponent } from './my-counter/my-counter.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, MyCounterComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
