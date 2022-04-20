b3055b0e99cba656c76b3d50a14b9ccb2c3cd91f
Toktam <ttmohebbi@gmail.com>
2022-04-20 17:09:28 -0400

    add styling

--- src/app/my-counter/my-counter.component.ts
+++ src/app/my-counter/my-counter.component.ts
@@ -6,6 +6,7 @@ import { increment, decrement, reset } from '../counter.actions';
 @Component({
   selector: 'app-my-counter',
   templateUrl: './my-counter.component.html',
+  styleUrls: ['./my-counter.component.css'],
 })
 export class MyCounterComponent {
   count$: Observable<number>;
